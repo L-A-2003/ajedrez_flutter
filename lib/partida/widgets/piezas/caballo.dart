662b17ef87a367ad22dd1eb0f0b344828b2fb3a1
Eduardo-Henderson <eduardo.henderson@estudiantes.utec.edu.uy>
2026-06-03 21:18:26 -0300

    movimiento de caballo y rey

--- lib/partida/widgets/piezas/caballo.dart
+++ lib/partida/widgets/piezas/caballo.dart
@@ -11,7 +11,24 @@ class Caballo extends Pieza {
   });
 
   @override
-  List<(int, int)> obtenerPosiblesMovimientos() {
-    return [];
-  }
+List<(int, int)> obtenerPosiblesMovimientos() {
+  final posibles = <(int, int)>[
+    (x + 1, y + 2),
+    (x + 1, y - 2),
+    (x - 1, y + 2),
+    (x - 1, y - 2),
+    (x + 2, y + 1),
+    (x + 2, y - 1),
+    (x - 2, y + 1),
+    (x - 2, y - 1),
+  ];
+
+  return posibles.where((movimiento) {
+    return movimiento.$1 >= 0 &&
+           movimiento.$1 < 8 &&
+           movimiento.$2 >= 0 &&
+           movimiento.$2 < 8;
+  }).toList();
+
+}
 }
